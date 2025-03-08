e6e5f1c56d7dfa1e747b540d769ea3ba2ac438d3
Sharjeel khan <iamkhansharjeel@gmail.com>
2025-03-08 20:29:04 +0500

    done

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+$server = "mysql:host=localhost;dbname=u681685764_arts_sharjeel";
+$username = "u681685764_sharjeel";
+$password = "t|7L5bVym";
+
+$pdo = new PDO($server, $username, $password);
